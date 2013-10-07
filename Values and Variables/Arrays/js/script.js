1bf70359e0644eedde61e0a541eda958cb3da77d
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-07 19:30:18 -0400

    arrays

---
+++ Values and Variables/Arrays/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created by tackleboxxdesigns on 10/7/13.
+ */
+// Arrays
+
+var muppetNames = ["Kermit", "Miss Piggy"]; // 0,1,2,3
+
+muppetNames[1] = "rizzo"; // Change index 1 to rizzo
+muppetNames[2] = "Fozzy"; //
+
+var num = 1;
+
+console.log(muppetNames[num]);
