e311a1e7538aa0e71d4d3d70c9c551bf56752f52
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-07 19:07:31 -0400

    String and Boolean Variables

---
+++ Values and Variables/string and boolean variables/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tackleboxxdesigns on 10/7/13.
+ */
+// String and Boolean Variables
+
+var age = 31; // number variable
+
+var isSudent = false; // boolean variable
+
+var firstName = "Danny";
+
+var phrase = 'I don\'t know \n you\'ll have to speak with the professor'
