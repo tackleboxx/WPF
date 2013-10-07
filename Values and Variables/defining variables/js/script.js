9bc4a97b39850fb9da6a6778df366788c19a1198
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-07 18:53:59 -0400

    Defining Variables

---
+++ Values and Variables/defining variables/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by tackleboxxdesigns on 10/7/13.
+ */
+
+// Defining Variables
+
+var age; // declaration
+age = 31; // definition
+
+// storage container = value
+
+var yearOfBirth = 1981; // declaring and defining on the same line
