cb3a44ea583bba697b7e4338dc1bfd61a5ca4632
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-09 09:30:15 -0400

    Fixed CSS JS for setup

--- Expressions_worksheet/Dog Years/js/script.js
+++ Expressions_worksheet/Dog Years/js/script.js
@@ -1,4 +1,2 @@
-// JavaScript
+// JavaScript Dog Years | Robert Dunn | Expressions Worksheet | 10-9-13
 
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
