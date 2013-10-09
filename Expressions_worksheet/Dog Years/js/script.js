9cf3b72b9709bb0ca8e16be827ff6e9355dc43b2
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-09 10:19:57 -0400

    hmmm

--- Expressions_worksheet/Dog Years/js/script.js
+++ Expressions_worksheet/Dog Years/js/script.js
@@ -3,6 +3,6 @@
 var sparkysAge = 1;
 var dogYears = 7;
 
-var result = sparkysAge * dogYears;
+var result = "Sparky is" + String(sparkysAge) + "human years old which is" * String(dogYears) + "in dog years";
 
 console.log(result);
