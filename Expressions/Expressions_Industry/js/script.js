248f0b76dc3de1dd886c94dd8a86fa55abba77cc
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-10 17:46:25 -0400

    set up wacky code

--- Expressions/Expressions_Industry/js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Expressions Industry | Robert Dunn | 10-9-13
-
-// How long am I on a computer a week?
-
-var hoursWorked = prompt('How many hours do you work a week?');// actual hours worked
-var breaks = prompt('How many (hours) for breaks do you take for the week?');//deduct for breaks
-var otherBreaks = prompt('Do you take any other breaks(in hours)?');// deduct other breaks
-var actualHours = Number(hoursWorked) - (Number(breaks) + Number(otherBreaks));// calculations hours minus breaks
-console.log("I work on a computer" + " " + Number(actualHours) + " " + "hours a week");//print from console log
