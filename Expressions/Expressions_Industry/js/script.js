5bc4ea7c64e3ed50340daa3e4f0b216df59cfe56
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-10 17:32:28 -0400

    set actual wours worked

--- Expressions/Expressions_Industry/js/script.js
+++ Expressions/Expressions_Industry/js/script.js
@@ -5,3 +5,4 @@
 var hoursWorked = prompt('How many hours do you work a week?');
 var breaks = prompt('How many breaks do you take?');
 var otherBreaks = prompt('Do you take any other breaks?');
+var actualHours = hoursWorked - breaks + otherBreaks;
