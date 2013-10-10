ceec2714426d9069356a0f478cc449c66077de71
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-10 16:43:56 -0400

    set vars for miles drivin and added all together

--- Expressions/Expressions_Personal/js/script.js
+++ Expressions/Expressions_Personal/js/script.js
@@ -3,5 +3,8 @@
 //Average miles driven per year
 
 var milesFirstYear = prompt('How many miles did you drive in the first year?'); // returns miles drivin 1st year
-var milesSecondYear = prompt('How many miles did you drive in the Second year?');// returns miles drivin 2st year
-var milesThirdYear = prompt('How many miles did you drive in the third year?');// returns miles drivin 3st year
+var milesSecondYear = prompt('How many miles did you drive in the Second year?');// returns miles drivin 2nd year
+var milesThirdYear = prompt('How many miles did you drive in the third year?');// returns miles drivin 3rd year
+var totalMiles = Number(milesFirstYear) + Number(milesSecondYear) + Number(milesThirdYear); //total miles drivin
+
+console.log(Number(totalMiles));
