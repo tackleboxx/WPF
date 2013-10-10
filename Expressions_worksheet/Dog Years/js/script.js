78d6841cbf8374a81ff1226514dcca206dd9709f
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-10 14:46:33 -0400

    added problem to code

--- Expressions_worksheet/Dog Years/js/script.js
+++ Expressions_worksheet/Dog Years/js/script.js
@@ -1,6 +1,20 @@
 // JavaScript Dog Years | Robert Dunn | Expressions Worksheet | 10-9-13
 
-var sparkysAge = 1;
+/*Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
+ Givens:
+
+ Sparky’s age.
+
+ Result Varable:
+
+ Sparky’s age in dog years.
+
+ Result to Print:
+
+ “Sparky is X human years old which is X in dog years.
+*/
+
+ var sparkysAge = 1;
 var dogYears = 7;
 
 var result = "Sparky is" + sparkysAge + "human years old which is" + dogYears + "in dog years";
