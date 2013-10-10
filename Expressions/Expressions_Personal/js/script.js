e693d8c20fe43ca1fa3af75a75622434c4dcdc7e
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-10 16:35:33 -0400

    added vars to personal expressions

--- Expressions/Expressions_Personal/js/script.js
+++ Expressions/Expressions_Personal/js/script.js
@@ -1,2 +1,7 @@
 // JavaScript Evpressions Personal | Robert Dunn | Expressions Worksheet | 10-9-13
 
+//Average miles driven per year
+
+var milesFirstYear = prompt('How many miles did you drive in the first year?'); // returns miles drivin 1st year
+var milesSecondYear = prompt('How many miles did you drive in the Second year?');// returns miles drivin 2st year
+var milesThirdYear = prompt('How many miles did you drive in the third year?');// returns miles drivin 3st year
