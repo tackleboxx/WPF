6e79b793b12b54aba1a62b5221ed3bbbe7beddce
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-14 12:02:32 -0400

    added stuff your face 1

--- Conditionals_worksheet/stuff your face 1/js/script.js
+++ Conditionals_worksheet/stuff your face 1/js/script.js
@@ -1,25 +1,14 @@
-// JavaScript Dog Years | Robert Dunn | Expressions Worksheet | 10-9-13
+// JavaScript Stuff your face 1 | Robert Dunn | Expressions Worksheet | 10-14-13
 
-/*Dogs age 7 times faster than humans so a dog that is 1 year old in human years is 7 years old in “dog years.”  Calculate how old Sparky the pit bull is in dog years based on his actual age.
- Givens:
+/* Stuff your face I
 
- Sparky’s age.
+ To get in the heavyweight division of the Strawberry Festival’s pie eating contest you must weigh 250 lbs or more.  Determine whether an entrant qualifies based on his weight.
 
- Result Varable:
+ Given:
+ Competitor’s weight.
+ Result:
+ “The competitor qualifies for the heavyweight division.” Or  “ The competitor needs to gain some weight!”
 
- Sparky’s age in dog years.
+ */
 
- Result to Print:
-
- “Sparky is X human years old which is X in dog years.
-*/
-
-var humanYears = 1; // defines how a human year to a dog year
-var dogYears = 7; //defines actual dog years to human year
-
-var sparkysAge = humanYears * dogYears; //multiplys human years to dog years to get how hold dog is in human years
-
-var result = "Sparky is" + " "  + sparkysAge + " " + "human years old which is" + " " + dogYears + " " + "in dog years";//actual to print
-
-console.log(result);// prints results
 
