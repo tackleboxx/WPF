8c9d6a5e26ae7d603540f727f652df48f6800386
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-14 12:17:38 -0400

    added Celsius to Fahrenheit converter

---
+++ Conditionals_worksheet/Celsius to Fahrenheit converter/js/script.js
@@ -0,0 +1,17 @@
+// JavaScript Celsius to Fahrenheit converter | Robert Dunn | Expressions Worksheet | 10-14-13
+
+/* Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered. If the user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit, the calculator should convert to Fahrenheit.
+
+
+ Given:
+ Degrees (in F or C)
+ Unit (a string holding an “F” or a “C”)
+ Result:
+ “The temperature is X degrees Celsius.” Or “The temperature is X degrees Fahrenheit.”
+ Data Sets to Test: (Note that data sets are not the only numbers that should work with your code.)
+ 32F is 0C
+ 100C is 212F
+ 90F is 32.22C
+
+ */
+
