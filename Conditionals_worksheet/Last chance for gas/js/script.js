c2ab6c98cd543e81e6f9a6f201d73e2dc28fe343
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-16 19:05:22 -0400

    added last chance for gas

---
+++ Conditionals_worksheet/Last chance for gas/js/script.js
@@ -0,0 +1,17 @@
+// JavaScript Last chance for gas | Robert Dunn | Conditionals Worksheet | 10-14-13
+
+/* Last Chance for Gas!
+
+ A driver has to determine if they can make it across the desert with their current fuel.  They are about to past the last gas station for the next 200 miles and they need to determine whether they should stop now for gas or not.
+
+
+ Given:
+ Gas efficiency of the car (in mpg)
+ Gauge reading of the gas tank (in %)
+ Car’s gas tank capacity (in gallons)
+ Result To Print Out:
+ “Yes, you can make it without stopping for gas!” or “You only have X gallons of gas in your tank, better get gas now while you can!”
+
+
+ */
+
