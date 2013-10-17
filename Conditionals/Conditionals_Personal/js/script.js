1890365a93bd13fcda6016d5bdc50ae470d7a487
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-17 17:53:35 -0400

    added comments for personal to show what was going on

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -8,8 +8,8 @@ var budget = 45; // Setting a budget for video game
 var videoGame = 60; // actual video game price
 var payCheck = 200; // money received from paycheck
 
-if (budget === videoGame || payCheck >= videoGame) { 
-    console.log("You can get the game!");
-}else {
-    console.log("You need more money!")
+if (budget === videoGame || payCheck >= videoGame) { // if budget equals price of video game or paycheck is greater or equal to video game, I get game
+    console.log("You can get the game!");// print that I get game
+}else { // otherwise
+    console.log("You need more money!")// print i need more money
 }
