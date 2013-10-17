4fd5b74f8ec616461074a27a94bb26bddc441ae1
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-17 18:33:14 -0400

    set wacky js

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -1,25 +1,6 @@
-// JavaScript Conditionals_Industry | Robert Dunn | Conditionals Worksheet | 10-14-13
+// JavaScript Conditionals_Wacky | Robert Dunn | Conditionals Worksheet | 10-14-13
 
-/* Can I travel to the meeting in california alone
+/* How many miles to oil change
 
  */
 
-var yearBorn = prompt('What year was you born?'); // get users date of birth
-var currentYear = prompt('What year is it?'); // get current year
-var flyAloneAge = 18;// the age you can fly alone
-var actualAge = currentYear - yearBorn;// calculate actual age by subtracting year born from current year
-actualAge;
-
-/*
-
-if (actualAge > flyAloneAge ) {
-    console.log("you can fly alone!");
-}else {
-    console.log("You need an adult to fly with you");
-}
-
-*/
-
-// if actual age is greater tha fly age then print you can fly alone
-//otherwise you need an adult
-(actualAge > flyAloneAge) ? console.log("you can fly alone!") : console.log("You need an adult to fly with you");
