93ce5ebeb8f4dd88d92131980ac4a745a3631dcf
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-16 20:21:27 -0400

    added movie ticket price

---
+++ Conditionals_worksheet/Movie Ticket Price/js/script.js
@@ -0,0 +1,15 @@
+// JavaScript Movie Ticket Price | Robert Dunn | Conditionals Worksheet | 10-14-13
+
+/* Movie Ticket Price
+
+ The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
+ Determine which of the two prices the customer is eligible for.
+
+ Given:
+ Time of Movie (Assume whole numbers here)
+ Age of the customer
+ Result To Print Out:
+ “The ticket price is X”
+
+ */
+
