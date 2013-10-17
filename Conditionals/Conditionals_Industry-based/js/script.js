73d2b3a00530798275ca418a6bc136f2a13afe7c
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-17 18:27:41 -0400

    added comments to js

--- Conditionals/Conditionals_Industry-based/js/script.js
+++ Conditionals/Conditionals_Industry-based/js/script.js
@@ -6,8 +6,8 @@
 
 var yearBorn = prompt('What year was you born?'); // get users date of birth
 var currentYear = prompt('What year is it?'); // get current year
-var flyAloneAge = 18;
-var actualAge = currentYear - yearBorn;
+var flyAloneAge = 18;// the age you can fly alone
+var actualAge = currentYear - yearBorn;// calculate actual age by subtracting year born from current year
 actualAge;
 
 /*
