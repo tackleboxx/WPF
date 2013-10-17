f48802062342fceb9eb3d03419b50ea207ebffa7
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-17 18:46:39 -0400

    added else if statemtn

--- Conditionals/Conditionals_Wacky/js/script.js
+++ Conditionals/Conditionals_Wacky/js/script.js
@@ -8,9 +8,11 @@ var milesDriven = 2500;
 var oilChange = 3000;
 var result = oilChange - milesDriven;
 
-if (milesDriven <= oilChange) {
-    console.log("You don't need an oil change");
+if (result >= oilChange) {
+    console.log("You need an oil change");
 }
 if(result < oilChange || result > 2500) {
     console.log("You should think about getting an oil change");
+}else if(result < oilChange) {
+    console.log("You don't need an oil change");
 }
