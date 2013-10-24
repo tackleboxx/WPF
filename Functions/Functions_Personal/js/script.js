fd5179e21cc6cead91eb855d1f074d10d2f396f2
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-24 16:52:12 -0400

    set comments for personal and finished

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -4,16 +4,16 @@
 
  */
 
-// Calculate my BMI
+// Calculate my BMI (body mass index)   
 
-var height = prompt('How tall are you? (in inches)');
-var weight = prompt('How much do you weigh? (in pounds)');
-var bmiCalc = 703;
-var total = calcBmi();
+var height = prompt('How tall are you? (in inches)');// prompt for height
+var weight = prompt('How much do you weigh? (in pounds)'); // prompt for weight
+var bmiCalc = 703; // calculation for bmi
+var total = calcBmi();//run total var
 
-function calcBmi(h, w, b) {
-    var bmi = height * height;
-    return bmi;
+function calcBmi(h, w, b) {// start of function
+    var bmi = weight/(height * height) * bmiCalc; // calculation for function
+    return bmi; // return bmi
 }
 
-console.log("My BMI is " + total);
+console.log("My BMI is " + total);// print to console
