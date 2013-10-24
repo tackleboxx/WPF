b78af37e1b12ebc2aef6a260f8eab9778e50f677
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-24 15:28:34 -0400

    set up js file for personal

--- Functions/Functions_Personal/js/script.js
+++ Functions/Functions_Personal/js/script.js
@@ -1,34 +1,8 @@
-// JavaScript Check the Login | Robert Dunn | Conditionals Worksheet | 10-14-13
+// JavaScript Functions_Personal | Robert Dunn | 10-24-13
 
-/* Check the Login
-
- Make sure the user has the correct username and password. If the username doesn’t match then a specific message for that should be printed to the console. If the password doesn’t match a message should appear for that, etc. Only one error message should appear.
-
- Given:
-
- Username entered by user
- Password entered by user
- Correct username
- Correct password
-
- Result To Print Out:
- “Welcome, (place their username here)!”  - if the username and password is correct
- “User not found. Try again.” -if the username does not match
- “Password does not match our records.”  -if the username matches but the password does not
+/* Functions Personal
 
  */
 
-var username = prompt('Username'); // username
-var password = prompt('Password'); // password
-var correctUsername = "Danny"; // correct username
-var correctPassword = "Danny"; // correct password
+// Calculate my BMI
 
-
-if(username === correctUsername && password === correctPassword) { //if the username and password is correct
-    console.log("Welcome," + " " + correctUsername + " " + "!");
-}
-if (username === correctUsername && password != correctPassword) { //if the username matches but the password does not
-    console.log("Password does not match our records.");
-}else if(username != correctUsername) {                             //if the username does not match
-    console.log("User not found. Try again.");
-}
