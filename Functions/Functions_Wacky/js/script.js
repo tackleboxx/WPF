9c4dd212b4e663697841df000179e91d1309ad2c
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-24 17:52:04 -0400

    added comments

--- Functions/Functions_Wacky/js/script.js
+++ Functions/Functions_Wacky/js/script.js
@@ -6,12 +6,12 @@
 
 // how fast am i going in kilometers?
 
-var miles = prompt('How fast are you going in miles per hour?');
-var kilometers = 1.6;
-var calcKilo = function(miles, kilometers){
-    var convert = miles * kilometers;
-    return convert;
+var miles = prompt('How fast are you going in miles per hour?'); // prompt for miles per hour
+var kilometers = 1.6; // kilo to miles
+var calcKilo = function(miles, kilometers){ //setting up function
+    var convert = miles * kilometers; "setting up the conversion"
+    return convert; // returned conversion
 }
 
-var actualKilo = calcKilo(miles,kilometers);
-console.log("You are going " + actualKilo + " kilometers per hour");
+var actualKilo = calcKilo(miles,kilometers); //invoking
+console.log("You are going " + actualKilo + " kilometers per hour"); //printing to console
