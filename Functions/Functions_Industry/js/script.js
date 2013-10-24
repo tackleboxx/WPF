f43bb28f7427982c05e42716819c041af539bdf0
tackleboxx <tackleboxxdesigns@gmail.com>
2013-10-24 16:57:01 -0400

    set up js file

--- Functions/Functions_Industry/js/script.js
+++ Functions/Functions_Industry/js/script.js
@@ -1,19 +1,8 @@
-// JavaScript Functions_Personal | Robert Dunn | 10-24-13
+// JavaScript unctions_Industry | Robert Dunn | 10-24-13
 
 /* Functions Personal
 
  */
 
-// Calculate my BMI (body mass index)   
+// How many square feet of material will i need for my roof
 
-var height = prompt('How tall are you? (in inches)');// prompt for height
-var weight = prompt('How much do you weigh? (in pounds)'); // prompt for weight
-var bmiCalc = 703; // calculation for bmi
-var total = calcBmi();//run total var
-
-function calcBmi(h, w, b) {// start of function
-    var bmi = weight/(height * height) * bmiCalc; // calculation for function
-    return bmi; // return bmi
-}
-
-console.log("My BMI is " + total);// print to console
